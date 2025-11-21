4139161cc3531f1e3acc4234bc2b73cc3ec20f62
LeeHwon0217 <hubo0217@naver.com>
2025-11-21 09:35:24 +0900

    초기 세팅

---
+++ app.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import postRouter from "./router/posts.mjs";
+import authRouter from "./router/auth.mjs";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/post", postRouter);
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.listen(8080, () => {
+  console.log("서버 실행중!");
+});
