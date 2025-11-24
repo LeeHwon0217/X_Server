e65cf05b3c54f01b7033bf656cecf5914716d16f
LeeHwon0217 <hubo0217@naver.com>
2025-11-24 15:23:13 +0900

    MySQL 적용 완료

--- router/posts.mjs
+++ router/posts.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import * as postController from "../controller/post.mjs";
 import { body } from "express-validator"; // 괄호가 없으면 export default를 가져옴. {필수}
 import { validate } from "../middleware/validator.mjs";
+import { isAuth } from "../middleware/auth.mjs";
 
 const router = express.Router();
 
@@ -18,23 +19,23 @@ const validatePost = [
 // http://127.0.0.1:8080/post
 // http://127.0.0.1:8080/post?userid=1
 // 콜백으로 함수를 실행하는 것이 아닌 주소 주기
-router.get("/", postController.getPosts);
+router.get("/", isAuth, postController.getPosts);
 
 // 글번호에 대한 포스트 가져오기
 // http://127.0.0.1:8080/post/:id
-router.get("/:id", postController.getPost);
+router.get("/:id", isAuth, postController.getPost);
 
 // 포스트 쓰기
 // http://127.0.0.1:8080/post
 // validatePost를 거쳐서 통과한 애만 넘어감
-router.post("/", validatePost, postController.createPost);
+router.post("/", isAuth, validatePost, postController.createPost);
 
 // 포스트 수정하기
 // http://127.0.0.1:8080/post/:id
-router.put("/:id", validatePost, postController.updatePost);
+router.put("/:id", isAuth, validatePost, postController.updatePost);
 
 // 포스트 삭제하기
 // http://127.0.0.1:8080/post/:id
-router.delete("/:id", postController.deletePost);
+router.delete("/:id", isAuth, postController.deletePost);
 
 export default router;
